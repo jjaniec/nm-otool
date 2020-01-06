8515fa87026eca82e44de9e1a9a5a780f4d71551
Joffrey Janiec <jjaniec@student.42.fr>
2020-01-06 20:05:42 +0100

    Begin multi arguments for nm & otool

--- srcs/ft_nm.c
+++ srcs/ft_nm.c
@@ -6,30 +6,40 @@
 /*   By: jjaniec <jjaniec@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/29 15:45:19 by jjaniec           #+#    #+#             */
-/*   Updated: 2019/12/20 19:56:18 by jjaniec          ###   ########.fr       */
+/*   Updated: 2020/01/06 19:44:12 by jjaniec          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <ft_nm.h>
 
-static char			*check_args(int ac, char **av)
+static int			read_and_process_file(char *filename)
 {
-	if (ac > 1)
-		return (av[1]);
-	return "a.out";
-}
-
-int					main(int ac, char **av)
-{
-	char	*file_path;
-	char	*file_content;
 	t_ft_nm_file	file;
+	char			*file_content;
 
-	file_path = check_args(ac, av);
-	if ((file.totsiz = read_file_content(file_path, &file_content)) == -1)
+	if ((file.totsiz = read_file_content(filename, &file_content)) == -1)
 		return (1);
 	dprintf(2, "Read file total size: %d\n", file.totsiz);
 	file.content = file_content;
 	file.seek_ptr = file.content;
 	return (ft_nm_process_file(&file));
+}
+
+int					main(int ac, char **av)
+{
+	int				i;
+	int				r;
+
+	if (ac == 1)
+		return (read_and_process_file("a.out"));
+	i = 0;
+	while (++i != ac)
+	{
+		if (ac != 2)
+			ft_printf("\n%s:\n", av[i]);
+		if ((r = read_and_process_file(av[i])))
+			return (r);
+		// if (ac != 2)
+	}
+	return (0);
 }
--- srcs/ft_otool.c
+++ srcs/ft_otool.c
@@ -6,35 +6,34 @@
 /*   By: jjaniec <jjaniec@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/20 18:07:35 by jjaniec           #+#    #+#             */
-/*   Updated: 2019/12/20 18:20:12 by jjaniec          ###   ########.fr       */
+/*   Updated: 2020/01/06 19:38:14 by jjaniec          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <ft_nm.h>
 
-static char			*check_args(int ac, char **av)
-{
-	if (ac > 1)
-		return (av[1]);
-	return (NULL);
-}
-
 int		main(int ac, char **av)
 {
-	char	*file_path;
-	char	*file_content;
+	char			*file_path;
+	char			*file_content;
 	t_ft_nm_file	file;
+	unsigned int	i;
 
-	if (!(file_path = check_args(ac, av)))
+	if (!(ac > 1))
 	{
 		ft_putstr_fd("ft_otool: at least one file must be specified\n", \
 			STDERR_FILENO);
 		return (1);
 	}
-	if ((file.totsiz = read_file_content(file_path, &file_content)) == -1)
-		return (1);
-	dprintf(2, "Read file total size: %d\n", file.totsiz);
-	file.content = file_content;
-	file.seek_ptr = file.content;
-	return (ft_otool_process_file(&file));
+	i = 0;
+	while (++i != ac)
+	{
+		file_path = av[i];
+		if ((file.totsiz = read_file_content(file_path, &file_content)) == -1)
+			return (1);
+		dprintf(2, "Read file total size: %d\n", file.totsiz);
+		file.content = file_content;
+		file.seek_ptr = file.content;
+		return (ft_otool_process_file(&file));
+	}
 }
